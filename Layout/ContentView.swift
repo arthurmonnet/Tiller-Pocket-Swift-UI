a634b5c61fd1694aa53322bafa84bbeb701ff51a
arthurmonnet <pro@arthurmonnet.com>
2020-03-31 17:38:24 +0200

    Initial Commit

---
+++ Layout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Layout
+//
+//  Created by Arthur on 31/03/2020.
+//  Copyright © 2020 Arthur Monnet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
