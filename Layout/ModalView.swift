485c5426900048f76e18fcf524428c1bc61922c4
arthurmonnet <pro@arthurmonnet.com>
2020-04-07 15:56:23 +0200

    Added QGrid Swift Extension for Collection Layout

--- Layout/ModalView.swift
+++ Layout/ModalView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ModalView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        HStack(spacing: 0) {
+            Product()
+            Product()
+            Product()
+            Product()
+        }
+        
     }
 }
 
@@ -19,3 +26,31 @@ struct ModalView_Previews: PreviewProvider {
         ModalView()
     }
 }
+
+struct Product: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .center) {
+                Spacer()
+                Text("Poulet ")
+                    .font(.system(size: 17, weight: .semibold))
+                    .multilineTextAlignment(.center)
+                    .offset(y: 8)
+                    .foregroundColor(.white)
+                Spacer()
+                Text("6,00 €")
+                    .font(.system(size: 15, weight: .regular))
+                    .offset(y: -8)
+                    .foregroundColor(.white)
+
+            }
+            .frame(height: 116)
+            .frame(width: screen.width / 2)
+            .background(Color("Primary"))
+        }
+    }
+}
+
+let screen = UIScreen.main.bounds
+
+
